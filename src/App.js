318edcfe30c85b4af69985da2c852a6ffe96bff0
artinalushaku <al64778@ubt-uni.net>
2024-12-23 00:01:07 +0100

    added home.jsx and fixed routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-
+import Home from './components/Home';
 
 const App = () => {
   return (
@@ -10,6 +10,7 @@ const App = () => {
         <Routes>
          
           <Route path="/nav" element={<Nav />} />
+          <Route path="/" element={<Home />} />
 
         </Routes>
       </div>
