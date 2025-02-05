fe1032cd695735892afcd5ab296d4d21f8ee8c9a
ra68288 <ra68288@ubt-uni.net>
2025-02-05 02:15:16 +0100

    Revert "added a new component folder"
    This reverts commit 478cb47f2a414146c6fab962e46ba2a0cae49c67, reversing
    changes made to e6e24e6215d3a3172a0be8e483c19851e9973747.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation  } from 'react-router-dom';
-import axios from 'axios';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-
-//Components import
 import Home from './components/Home/Home';
 import Login from './components/LoginRegister/Login';
 import Register from './components/LoginRegister/Register';
@@ -20,98 +17,28 @@ import Nav from './components/layout/nav';
 import Dubai from './components/Dubai/Dubai';
 import VisitKosova from './components/VisitKosova/VisitKosova';
 
-import Search from './components/Search/Search';
-
-import ProtectedRoute from './aDashboard/ProtectedRoute';
-
-//Dashboard import
-import Dashboard from './aDashboard/Dashboard';
-import AddUser from './aDashboard/users/AddUser';
-import ManageUser from './aDashboard/users/ManageUser';
-import ManageHomeTable from './aDashboard/tables/ManageHomeTable';
-import GreqiSlider from './aDashboard/tables/GreqiSlider';
-import TurqiTable from './aDashboard/tables/TurqiTable';
-import DubaiTable from './aDashboard/tables/DubaiPricesTable';
-import SliderManage from './aDashboard/tables/SliderManage';
-import ManageCardsStamboll from './aDashboard/tables/CardsStamboll';
-import HurghadaCards from './aDashboard/tables/HurghadaCards';
-import KapodakiaCards from './aDashboard/tables/KapodakiaCards';
-
-import AddAranzhmanet from './aDashboard/aranzhmanet/AddAranzhmanet';
-import ManageAranzhmanet from './aDashboard/aranzhmanet/ManageAranzhmanet';
 
 
-
-import AddShtetin from './aDashboard/shtetet/AddShtetin';
-import ManageShtetet from './aDashboard/shtetet/ManageShtetet';
-
-import AddQytetet from './aDashboard/qytetet/AddQytetet';
-import ManageQytetet from './aDashboard/qytetet/ManageQytetet';
-
-
-import ManageLogs from './aDashboard/Logs/Logs';
-
-
-import Kontakt from './components/Kontakt/Kontakt';
 
 const App = () => {
- 
-  const location = useLocation();
-  const isHomeroute = !location.pathname.startsWith('/dashboard') 
   return (
-    <>
-      {isHomeroute && <Nav />}
-      
-      
-        <div className='overflow-hidden'>
-          <Routes>
-            <Route path="/" element={<Home />} />
+    <Router>
+      <div>
+        <Routes>
+         
+        <Route path="/" element={<Home />} />
             <Route path="/turqi" element={<Turqi />} />
             <Route path="/bullgari" element={<Bullgari />} />
             <Route path="/greqi" element={<Greqi />} />
-            <Route path="/Kontakt" element={<Kontakt />} />
+            <Route path="/Kontakt" element={<kontakt />} />
             <Route path="/maqedoni" element={<Maqedoni />} />
             <Route path="/festateFundvitit" element={<FestateFundvitit />} />
             <Route path="/europecitybreak" element={<EuropeCityBreak />} />
-            <Route path="/dashboard" element={<ProtectedRoute><Dashboard /> </ProtectedRoute>} />
-            <Route path="/dashboard/AddUser" element={<ProtectedRoute><AddUser /></ProtectedRoute>} />
-            <Route path="/dashboard/ManageUser" element={<ProtectedRoute><ManageUser /></ProtectedRoute>} />
-          
 
-            <Route path="/dashboard/Logs" element={<ProtectedRoute><ManageLogs /></ProtectedRoute>} />
-           
-
-
-            <Route path="/dashboard/AddAranzhmanet" element={<ProtectedRoute><AddAranzhmanet /></ProtectedRoute>} />
-            <Route path="/dashboard/ManageAranzhmanet" element={<ProtectedRoute><ManageAranzhmanet /></ProtectedRoute>} />
-
-         
-
-            <Route path="/dashboard/AddShtetin" element={<ProtectedRoute><AddShtetin /></ProtectedRoute>} />
-            <Route path="/dashboard/ManageShtetet" element={<ProtectedRoute><ManageShtetet /></ProtectedRoute>} />
-
-            <Route path="/dashboard/AddQytetet" element={<AddQytetet />} />
-            <Route path="/dashboard/ManageQytetet" element={<ManageQytetet />} />
-
-            <Route path="/Search" element={<Search />} />
-
-            <Route path="/Dubai" element={<Dubai />} />
-            <Route path="/VisitKosova" element={<VisitKosova />} />
-
-            {/* <Route path="/AddRoomPrices" element={<AddRoomPrices />} /> */}
-
-
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-
-          </Routes>
-      </div></>
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
-const AppWrapper = () => (
-  <Router>
-    <App />
-  </Router>
-);
-export default AppWrapper;
+export default App;
