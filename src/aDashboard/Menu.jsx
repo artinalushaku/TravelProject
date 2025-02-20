b6f726ae331aacea1f9fcd6af4d93aea0629ce05
artinalushaku <al64778@ubt-uni.net>
2025-02-21 00:28:50 +0100

    removed greqiSlider

--- src/aDashboard/Menu.jsx
+++ src/aDashboard/Menu.jsx
@@ -99,14 +99,7 @@ function Menu() {
             </Link>
           </li>
 
-          <li>
-            <Link
-              to="/dashboard/GreqiSlider"
-              className="block px-4 py-2 text-blue-500 font-bold hover:text-blue-700 hover:bg-gray-100"
-            >
-              GreqiSlider
-            </Link>
-          </li>
+      
 
           <li>
             <Link
